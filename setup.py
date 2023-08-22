860bbd6cc014ec559feec6bd3d61b29a00814ade
unknown <be.sohelz@gmail.com>
2023-08-22 21:51:48 +0530

    Flask Server, Git , heroku, docker setup test 1

--- setup.py
+++ setup.py
@@ -1,27 +1,35 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
+#Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.3"
+AUTHOR="Sohel  Jagirdar"
+DESRCIPTION="This  Machine Learning Project for data pipeline using Github , Docker , Heroku with Github Action"
 
-# Declaring Variable for setup funtion
-PROJECT_NAME = 'housing predictor'
-VERSION ='0.0.1'
-AUTHOR= 'Sohel'
-DESCRIPTION = 'Housing Prediction Using CICD Pipeline , Github, Docker with Heroku '
-PACKAGES=['Housing']
-REQUIREMENTS_FILE_NAME= 'requirements.txt'
+REQUIREMENT_FILE_NAME="requirements.txt"
 
-def get_requirements_list()->List[str]:
-    '''This funtion going to return requirement from text file'''
-    with open(REQUIREMENTS_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+
+
+
+def get_requirements_list() -> List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+    return This function is going to return a list which contain name
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        return requirement_list
+
+
 
 setup(
 name=PROJECT_NAME,
-version =VERSION,
-author = AUTHOR,
-Description = DESCRIPTION,
-Packages=PACKAGES,
-install_requirement=get_requirements_list()
+version=VERSION,
+author=AUTHOR,
+description=DESRCIPTION,
+packages=find_packages(),
+install_requires=get_requirements_list()
 )
-
-
